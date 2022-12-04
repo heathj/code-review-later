0b81c865f3de40d55105fb380a96d8bbda368829
Jake Heath <jheath@chanzuckerberg.com>
2022-12-04 14:35:48 -0700

    first try

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,85 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {GetResponseDataTypeFromEndpointMethod} from '@octokit/types'
+import moment from 'moment'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const amountStr = core.getInput('amount')
+    const amount = parseInt(amountStr, 10)
+    if (isNaN(amount)) {
+      core.setFailed('invalid minCodeReviewTime, please set to number')
+      return
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const unit = core.getInput('unit')
+    if (unit !== 'h' && unit !== 'd') {
+      core.setFailed('invalid unit, only "h" or "d" accepted')
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const {owner, repo} = github.context.repo
+    const unreviewedPRs = await getUnreviewedPRsSince(owner, repo, amount, unit)
+    if (unreviewedPRs.length >= 0) {
+      core.info(
+        `The unreviewed PRs: ${JSON.stringify(unreviewedPRs.map(u => u.id))}`
+      )
+      core.setFailed('There are closed PRs that need code review')
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+type OctokitReturnType = ReturnType<typeof github.getOctokit>
+function getOctokit(): OctokitReturnType {
+  const token = core.getInput('token')
+  return github.getOctokit(token)
+}
+
+async function getUnreviewedPRsSince(
+  owner: string,
+  repo: string,
+  amount = 5,
+  unit: 'h' | 'd' = 'h',
+  config: {
+    octokit: OctokitReturnType
+  } = {
+    octokit: getOctokit()
+  }
+): Promise<
+  GetResponseDataTypeFromEndpointMethod<typeof config.octokit.rest.pulls.list>
+> {
+  let unreviewed: GetResponseDataTypeFromEndpointMethod<
+    typeof config.octokit.rest.pulls.list
+  > = []
+
+  const iter = config.octokit.paginate.iterator(
+    config.octokit.rest.pulls.list,
+    {
+      owner,
+      repo,
+      state: 'closed'
+    }
+  )
+
+  for await (const pulls of iter) {
+    const old = pulls.data.filter(p => {
+      if (!p.merged_at) {
+        return false
+      }
+      return isDeltaAfterNow(p.merged_at, amount, unit)
+    })
+    unreviewed = [...unreviewed, ...old]
+  }
+  return unreviewed
+}
+
+function isDeltaAfterNow(a: string, amount: number, unit: 'h' | 'd'): boolean {
+  const now = moment()
+  const then = moment(a)
+  then.add(amount, unit)
+  return then.isSameOrAfter(now)
 }
 
 run()
